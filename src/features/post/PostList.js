0cd75b6d9492f5e94fe3194baf4b0d75001de203
usama <narusamanamikaze@gmail.com>
2023-04-11 04:26:44 +0500

    added axios to fetch posts from external source

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -1,32 +1,52 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
-
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchPosts,
+  getPostsError,
+  getPostsStatus,
+  selectAllPosts,
+} from "./postSlice";
+
+import PostExcerpt from "./PostExcerpt";
+
 const PostList = () => {
+  const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-  const orderPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+  React.useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+      console.log("here", posts);
+    }
+  }, [postsStatus, dispatch]);
 
-  const renderedPosts = orderPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postsStatus == "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b?.date?.localeCompare(a?.date));
+    content = orderedPosts?.map((post) => (
+      <PostExcerpt key={post?.id} post={post} />
+    ));
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  // const orderPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+
+  // const renderedPosts = orderPosts.map((post) => (
+  //   <PostExcerpt key={post.id} post={post} />
+  // ));
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,5 +1,8 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from "axios";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
 const REACTIONS_OBJECT = {
   thumbsUp: 0,
   wow: 0,
@@ -8,22 +11,24 @@ const REACTIONS_OBJECT = {
   coffee: 0,
 };
 
-const initialState = [
-  {
-    id: 1,
-    title: "learning",
-    content: "just stuff",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: REACTIONS_OBJECT,
-  },
-  {
-    id: 2,
-    title: "we're normal men",
-    content: "just innocent men",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: REACTIONS_OBJECT,
-  },
-];
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
+});
+
+const initialState = {
+  posts: [],
+  status: "idle",
+  error: null,
+};
+
+export const addNewPost = createAsyncThunk(
+  "posts/addNewPost",
+  async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost);
+    return response.data;
+  }
+);
 
 const postSlice = createSlice({
   name: "posts",
@@ -31,7 +36,7 @@ const postSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -48,14 +53,64 @@ const postSlice = createSlice({
     },
     reactionAdded: (state, action) => {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id == postId);
+      const existingPost = state.posts.find((post) => post.id == postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
     },
+    extraReducers(builder) {
+      builder
+        .addCase(fetchPosts.pending, (state, action) => {
+          state.status = "loading";
+        })
+        .addCase(fetchPosts.fulfilled, (state, action) => {
+          state.status = "succeeded";
+          // Adding date and reactions
+          let min = 1;
+          const loadedPosts = action.payload.map((post) => {
+            post.date = sub(new Date(), { minutes: min++ }).toISOString();
+            post.reactions = REACTIONS_OBJECT;
+            return post;
+          });
+
+          // Add any fetched posts to the array
+          state.posts = state.posts.concat(loadedPosts);
+        })
+        .addCase(fetchPosts.rejected, (state, action) => {
+          state.status = "failed";
+          state.error = action.error.message;
+        });
+      // .addCase(addNewPost.fulfilled, (state, action) => {
+      //   // Fix for API post IDs:
+      //   // Creating sortedPosts & assigning the id
+      //   // would be not be needed if the fake API
+      //   // returned accurate new post IDs
+      //   const sortedPosts = state.posts.sort((a, b) => {
+      //     if (a.id > b.id) return 1;
+      //     if (a.id < b.id) return -1;
+      //     return 0;
+      //   });
+      //   action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+      //   // End fix for fake API post IDs
+
+      //   action.payload.userId = Number(action.payload.userId);
+      //   action.payload.date = new Date().toISOString();
+      //   action.payload.reactions = {
+      //     thumbsUp: 0,
+      //     hooray: 0,
+      //     heart: 0,
+      //     rocket: 0,
+      //     eyes: 0,
+      //   };
+      //   console.log(action.payload);
+      //   state.posts.push(action.payload);
+      // });
+    },
   },
 });
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 export const { postAdded, reactionAdded } = postSlice.actions;
 export default postSlice.reducer;
