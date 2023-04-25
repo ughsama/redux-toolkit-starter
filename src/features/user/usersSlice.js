95a7b670fd89525e6b184a66e462e3104a7adc64
usama <narusamanamikaze@gmail.com>
2023-04-25 22:57:46 +0500

    fetch posts from json placeholder api and map to list

--- src/features/user/usersSlice.js
+++ src/features/user/usersSlice.js
@@ -1,24 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {
-    id: 0,
-    name: "hey",
-  },
-  {
-    id: 1,
-    name: "hey 1",
-  },
-  {
-    id: 2,
-    name: "hey 2",
-  },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return [...response.data];
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
